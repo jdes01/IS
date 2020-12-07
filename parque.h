2f51f5e1e02c65febd6f0deb8f85da3ad25db051
Alberto Cano <soniccano2@gmail.com>
2020-12-07 13:02:39 +0100

    Avance parque

---
+++ parque.h
@@ -0,0 +1,31 @@
+#ifndef PARQUE_H
+#define PARQUE_H
+
+#include <string>
+
+class Parque{
+
+    private:
+
+        std::string nombre_;
+        std::string id_;
+        std::string ubicacion_;
+        float superficie_;
+        std::list <Sendero> senderos_;
+
+    public:
+
+        Parque(std::string nombre, std::string ubicacion = "", float superficie = "");
+
+        inline 
+
+
+
+
+
+
+
+
+}:
+
+#endif
