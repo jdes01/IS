ff2f49bb000f6b277386760fea110316c6c4331a
Salsedini <d.s.022222@gmail.com>
2020-12-07 14:12:37 +0100

    constructor monitor y test

--- monitor.h
+++ monitor.h
@@ -1,7 +1,28 @@
 #ifndef MONITOR_H
 #define MONITOR_H
-#include "personas.h"
+#include "persona.h"
+#include "fecha.h"
+#include "nombre.h"
+#include <string>
+
+class Monitor : public Persona{
+    
+    private:
+
+        std::string id_;
+
+    private:
+
+        Monitor ( std::string id,
+                  std::string nombre,           std::string primerApellido,
+                  std::string segundoApellido,  std::string dni,
+                  std::string telefono,         int dia,
+                  int mes,                      int año, 
+                  std::string direccion,        std::string correo)
+                  
+                  :Persona(nombre, primerApellido, segundoApellido, dni, telefono, dia, mes, año, direccion, correo){id_=id;}
 
 
+};
 
 #endif
