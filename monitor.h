d87ccf15dfdeb22965eee0d52096c5dd418238f5
Salsedini <d.s.022222@gmail.com>
2020-12-09 19:22:31 +0100

    clase monitor completa

--- monitor.h
+++ monitor.h
@@ -3,6 +3,8 @@
 #include "persona.h"
 #include "fecha.h"
 #include "nombre.h"
+#include "sendero.h"
+#include "parque.h"
 #include <string>
 
 class Monitor : public Persona{
@@ -22,6 +24,13 @@ class Monitor : public Persona{
                   :Persona(nombre, primerApellido, segundoApellido, dni, telefono, dia, mes, ano, direccion, correo){id_="0";}
 
         inline std::string getId(){return id_;}
+        inline void setId(std::string id){id_=id;}
+        void crearIncidencia(Sendero sendero);
+        void editarIncidencia(std::string id);
+        void eliminarIncidencia(std::string id);
+        void crearRuta(Parque parque);
+        void editarRuta(std::string id);
+        void eliminarRuta(std::string id);
 
 
 };
