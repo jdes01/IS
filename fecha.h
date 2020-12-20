f25f40a18cac24a9ba482ae340e00eaaac9ce754
js <jasampa3000@gmail.com>
2020-12-20 17:51:01 +0100

    init admin cc y h

--- fecha.h
+++ fecha.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef FECHA_H
 #define FECHA_H
 struct fecha {
@@ -9,11 +8,3 @@ struct fecha {
 };
 
 #endif
-=======
-struct fecha {
-    
-    int dia;
-    int mes;
-    int ano;
-};
->>>>>>> be146fe0ca404552b76e390eab76df2db1a3187e
--- nombre.h
+++ nombre.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef NOMBRE_H
 #define NOMBRE_H
 #include <string>
@@ -11,13 +10,3 @@ struct nombre {
 };
 
 #endif
-=======
-#include <string>
-
-struct nombre {
-
-    std::string nombre;
-    std::string primerApellido;
-    std::string segundoApellido;
-};
->>>>>>> be146fe0ca404552b76e390eab76df2db1a3187e
