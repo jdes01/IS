c0295013bca0d6d39625bd6e5e720bbff4873f2e
Javier de Santiago <javier@MacBook-Air-de-Javier.local>
2020-12-21 17:37:51 +0100

    fix

--- admin.cc
+++ admin.cc
@@ -4,27 +4,6 @@
 #include <iostream>
 #include <string>
 
-Admin::Admin( std::string nombre,           std::string primerApellido,
-              std::string segundoApellido,  std::string dni,
-              std::string telefono,         int dia,
-              int mes,                      int ano, 
-              std::string direccion,        std::string correo){
-                       
-                    nombre_.nombre = nombre;
-                    nombre_.primerApellido = primerApellido;
-                    nombre_.segundoApellido = segundoApellido;
-
-                    dni_ = dni;
-                    telefono_ = telefono;
-
-                    fechaNacimiento_.dia = dia;
-                    fechaNacimiento_.mes = mes;
-                    fechaNacimiento_.ano = ano;
-                    
-                    direccion_ = direccion;
-                    correo_ = correo;
-                }
-
 void Admin::crearParque(){
 
     std::string nombre, ubicacion;
