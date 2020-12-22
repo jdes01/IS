cd5488e04887daca2869cc64bb5cab87726ae93f
Salsedini <d.s.022222@gmail.com>
2020-12-22 16:18:31 +0100

    Test ruta

---
+++ ruta_unittest.cc
@@ -0,0 +1,13 @@
+#include "ruta.h"
+#include "gtest/gtest.h"
+
+TEST(Ruta, Constructor) {
+  
+  Parque parque("a", "a", 1);
+  Ruta r("nombre", 1, parque);
+  
+  EXPECT_EQ( "Nombre", r.getNombre());
+  EXPECT_EQ( 1,  r.getAforo());
+  EXPECT_EQ( parque, r.getParque());
+
+}
